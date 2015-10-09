49d813812c1dd82d056d47b976422897b3d05349
Gabriel Candal <gabrielcandal@gmail.com>
2015-10-09 16:36:19 +0100

    Room set/get values.

---
+++ room.py
@@ -0,0 +1,66 @@
+from datetime import *
+
+class Room:
+    def __init__(self):
+        self.temperatures = {}
+        self.noises = {}
+        self.people = {}
+        self.previous_nr_people = 0
+
+    def set_temperature(self, time, adapter_id, temperature):
+        if adapter_id not in self.temperatures:
+            self.temperatures[adapter_id] = {}
+        self.temperatures[adapter_id][time] = temperature
+
+    def set_noise(self, time, adapter_id, noise):
+        if adapter_id not in self.noises:
+            self.noises[adapter_id] = {}
+        self.noises[adapter_id][time] = noise
+
+    def add_person(self, time, adapter_id):
+        if adapter_id not in self.people:
+            self.people[adapter_id] = {}
+        self.previous_nr_people += 1
+        self.people[adapter_id][time] = self.previous_nr_people
+
+    def remove_person(self, time, adapter_id):
+        if adapter_id not in self.people:
+            self.people[adapter_id] = {}
+        self.previous_nr_people -= 1
+        self.previous_nr_people = max(self.previous_nr_people, 0)
+        self.people[adapter_id][time] = self.previous_nr_people
+
+    def get_people(self):
+        return self.previous_nr_people
+
+    def get_temperature(self, at_time):
+        sum_temperatures = 0
+        count = 0
+        for (adapter_id, value) in self.temperatures.items():
+            latest_time = None
+            latest_temperature = None
+            for (entry_time, temperature) in value.items():
+                if not latest_time or entry_time > latest_time:
+                    latest_time = entry_time
+                    latest_temperature = temperature
+            if at_time > latest_time and at_time - latest_time < timedelta(hours=1):
+                sum_temperatures += latest_temperature
+                count += 1
+
+        return sum_temperatures / float(count) if count > 0 else None
+
+    def get_noise(self, at_time):
+        sum_noises = 0
+        count = 0
+        for (adapter_id, value) in self.noises.items():
+            latest_time = None
+            latest_noise = None
+            for (entry_time, temperature) in value.items():
+                if not latest_time or entry_time > latest_time:
+                    latest_time = entry_time
+                    latest_noise = temperature
+            if at_time > latest_time and at_time - latest_time < timedelta(hours=1):
+                sum_noises += latest_noise
+                count += 1
+
+        return sum_noises / float(count) if count > 0 else None
---
+++ room_test.py
@@ -0,0 +1,55 @@
+from room import Room
+from datetime import datetime, timedelta
+
+def test_all():
+    test_get_temperature()
+    test_get_people()
+    test_get_noise()
+
+
+def test_get_temperature():
+    room = Room()
+    
+    room.set_temperature(datetime.now(), 0, 20)
+    assert(room.get_temperature(datetime.now()) == 20)
+    
+    room.set_temperature(datetime.now(), 1, 30)
+    assert(room.get_temperature(datetime.now()) == 25)
+
+    room.set_temperature(datetime.now() + timedelta(hours=9), 0, 50)
+    assert(room.get_temperature(datetime.now() + timedelta(hours=9)) == 50)
+
+
+def test_get_people():
+    room = Room()
+
+    room.remove_person(datetime.now(), 0)
+    assert(room.get_people() == 0)
+
+    room.add_person(datetime.now(), 0)
+    assert(room.get_people() == 1)
+
+    room.remove_person(datetime.now(), 0)
+    assert(room.get_people() == 0)
+
+    room.add_person(datetime.now(), 0)
+    assert(room.get_people() == 1)
+
+    room.add_person(datetime.now(), 0)
+    assert(room.get_people() == 2)
+
+    room.remove_person(datetime.now(), 0)
+    assert(room.get_people() == 1)
+
+
+def test_get_noise():
+    room = Room()
+
+    room.set_noise(datetime.now(), 0, 20)
+    assert(room.get_noise(datetime.now()) == 20)
+
+    room.set_noise(datetime.now(), 1, 30)
+    assert(room.get_noise(datetime.now()) == 25)
+
+    room.set_noise(datetime.now() + timedelta(hours=1), 0, 30)
+    assert(room.get_noise(datetime.now() + timedelta(hours=1)) == 30)
